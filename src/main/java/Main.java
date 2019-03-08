efd31a1207d28529a0286ec9a6ce59a336dca9bd
Krzychu88 <wojcieszek.krzysztof@gmail.com>
2019-03-08 23:00:04 +0100

    drugaproba

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,11 @@ public class Main {
         Magazyn magazyn = new Magazyn();
 
         Pracownik pierwszy = new Pracownik("Kazik","SynStanislawa", 87956523,23,500);
+        Pracownik drugi = new Pracownik("s","d", 3423432,43,2000);
         magazyn.dodajPracownia(pierwszy);
+        magazyn.dodajPracownia(drugi);
         magazyn.zatrudnieni();
+
 
 
     }
